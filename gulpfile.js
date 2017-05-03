9255b2340c3fe556c779961ee867b954fd7797ac
Mark Monteiro <mark.monteiro23@gmail.com>
2017-05-03 12:56:52 -0400

    Use gulp for Jekyll build and for BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,36 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const child = require('child_process');
+const gutil = require('gulp-util');
+const browserSync = require('browser-sync').create();
+
+const siteRoot = '_site';
+
+// Jekyll Build
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', ['build',
+    '--watch',
+    '--incremental',
+    '--drafts'
+  ]);
+
+  const jekyllLogger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+});
 
 // BrowserSync static server
 gulp.task('browsersync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "_site"
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: siteRoot
+    },
+    files: [siteRoot + '/**']
+  });
 });
+
+gulp.task('default', ['jekyll', 'browsersync']);
