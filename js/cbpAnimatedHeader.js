8a5d75d3e77ede7d308496a1b846d523102cda0c
Mark Monteiro <mark.monteiro23@gmail.com>
2017-05-03 12:43:56 -0400

    fix CodeClimate errors

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -8,7 +8,7 @@
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
-window.cbpAnimatedHeader = (function() {
+(function() {
 
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-fixed-top' ),
