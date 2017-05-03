d24c65523360ac9fa9bf52c33a6983a42d80ce0c
Mark Monteiro <mark.monteiro23@gmail.com>
2017-05-03 11:53:14 -0400

    Fix CodeClimate errors

--- js/vertical-timeline.js
+++ js/vertical-timeline.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function($){
 
 	//hide timeline blocks which are outside the viewport
 	$timeline_blocks.each(function(){
-		if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
+		if(!inViewport(this)) {
 			$(this).find($hide_classes).addClass('hide-timeline');
 		}
 	});
@@ -12,12 +12,15 @@ jQuery(document).ready(function($){
 	//on scolling, show/animate timeline blocks when enter the viewport
 	$(window).on('scroll', function(){
 		$timeline_blocks.each(function(){
-			var in_viewport = $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75;
 			var is_hidden = $(this).find($hide_classes).hasClass('hide-timeline');
 
-			if( in_viewport && is_hidden ) {
+			if(is_hidden && inViewport(this)) {
 				$(this).find($hide_classes).removeClass('hide-timeline').addClass('bounce-in');
 			}
 		});
 	});
+
+	function inViewport(selector) {
+		return $(selector).offset().top <= $(window).scrollTop()+$(window).height()*0.75;
+	}
 });
