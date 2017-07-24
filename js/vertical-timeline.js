878ffb9851e3c24e3eec13cc3098a8ca79c1c658
Mark Monteiro <mark.monteiro23@gmail.com>
2017-07-24 15:59:03 -0400

    convert tabs to spaces

--- js/vertical-timeline.js
+++ js/vertical-timeline.js
@@ -1,26 +1,26 @@
 jQuery(document).ready(function($){
-	var $timeline_blocks = $('.cd-timeline-block');
-	var $hide_classes = '.cd-timeline-img, .cd-timeline-point, .cd-timeline-content';
+    var $timeline_blocks = $('.cd-timeline-block');
+    var $hide_classes = '.cd-timeline-img, .cd-timeline-point, .cd-timeline-content';
 
-	//hide timeline blocks which are outside the viewport
-	$timeline_blocks.each(function(){
-		if(!inViewport(this)) {
-			$(this).find($hide_classes).addClass('hide-timeline');
-		}
-	});
+    //hide timeline blocks which are outside the viewport
+    $timeline_blocks.each(function(){
+        if(!inViewport(this)) {
+            $(this).find($hide_classes).addClass('hide-timeline');
+        }
+    });
 
-	//on scolling, show/animate timeline blocks when enter the viewport
-	$(window).on('scroll', function(){
-		$timeline_blocks.each(function(){
-			var is_hidden = $(this).find($hide_classes).hasClass('hide-timeline');
+    //on scolling, show/animate timeline blocks when enter the viewport
+    $(window).on('scroll', function(){
+        $timeline_blocks.each(function(){
+            var is_hidden = $(this).find($hide_classes).hasClass('hide-timeline');
 
-			if(is_hidden && inViewport(this)) {
-				$(this).find($hide_classes).removeClass('hide-timeline').addClass('bounce-in');
-			}
-		});
-	});
+            if(is_hidden && inViewport(this)) {
+                $(this).find($hide_classes).removeClass('hide-timeline').addClass('bounce-in');
+            }
+        });
+    });
 
-	function inViewport(selector) {
-		return $(selector).offset().top <= $(window).scrollTop()+$(window).height()*0.75;
-	}
+    function inViewport(selector) {
+        return $(selector).offset().top <= $(window).scrollTop()+$(window).height()*0.75;
+    }
 });
