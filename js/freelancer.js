3095aff9918d93f9c0b37e25e14b46b46445ec88
Mark Monteiro <mark.monteiro23@gmail.com>
2019-12-26 20:11:06 +0100

    Update hash in URL when clicking on scrolling links

--- js/freelancer.js
+++ js/freelancer.js
@@ -4,24 +4,41 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
+    // Scroll to href hash targets on the same page
+    // NOTE: Requires jQuery Easing plugin
     $('body').on('click', '.page-scroll a', function(event) {
+        
+        // Skip handling if this browser does not support the URL API
+        if (typeof URL !== 'function') return;
+
+        // Get the target URL; do nothing if the target is on a different page
         var $anchor = $(this);
-        var $target = $($anchor.attr('href'));
+        var targetUrl = new URL($anchor.attr('href'));
+        if (window.location.pathname !== targetUrl.pathname) return;
+        
+        // Get the target element to scroll to; default to the top of the page
+        var $target = $(targetUrl.hash || '#page-top');
+        if (!$target || !$target.length) return;
+
+        // Scroll to target
         var scrollTop = $target.offset().top - $('.navbar-fixed-top .navbar-header').height();
         $('html, body').stop().animate({ scrollTop: scrollTop }, 1500);
         event.preventDefault();
+
+        // Update URL
+        window.location.hash = targetUrl.hash;
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 10 + $('.navbar-fixed-top .navbar-header').height()
+    });
+
+    // Close the Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
     });
 });
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top',
-    offset: 10 + $('.navbar-fixed-top .navbar-header').height()
-});
-
-// Close the Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
